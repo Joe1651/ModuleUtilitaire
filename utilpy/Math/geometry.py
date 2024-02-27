927878ad99862eb8c111de7291c705a2581cca97
Joe1651 <jonathan.duplantis@hotmail.com>
2024-02-27 14:29:51 -0500

    classe Forme, Rectangle et Cercle

--- utilpy/Math/geometry.py
+++ utilpy/Math/geometry.py
@@ -1,6 +1,6 @@
 import math
 from abc import abstractmethod, ABC
-from utilpy.Console.console_utils import *
+from Console.console_utils import *
 
 
 class Forme(ABC):
@@ -8,14 +8,47 @@ class Forme(ABC):
     def __init__(self):
         raise NotImplementedError("Une forme c'est abstrait, on ne peut pas en construire.")
 
+    @abstractmethod
     def aire(self):
         pass
 
+    @abstractmethod
     def périmètre(self):
         pass
 
+    @abstractmethod  # Ceci va forcer toutes les classes dérivées à overrider de __str__
+    def __str__(self):
+        pass
+
+    def __repr__(self):  # Ceci va forcer toutes les classes dérivées à utiliser le __str__ pour représenter l'instance
+        return self.__str__()
+
+
+class Cercle(Forme, ABC):
+    def __init__(self, rayon=1):
+        self.rayon = rayon
+
+    def aire(self):
+        return math.pi * self.rayon ** 2
+
+    def périmètre(self):
+        return 2 * math.pi * self.rayon
+
+    def __str__(self):
+        return (f"Ceci est un rayon de {self.rayon}m de rayon, avec une aire de {self.aire()}m2 et un périmètre de "
+                f"{self.périmètre()}m")
+
 
 class Rectangle(Forme, ABC):
+    @classmethod
+    def créer_rectangle_or(cls):
+        nb_or = (1 + math.sqrt(5)) / 2
+        return Rectangle(1, nb_or)
+
+    @classmethod
+    def créer_rect_construction(cls):
+        return Rectangle(3, 4)
+
     # CONSTRUCTEUR
     def __init__(self, longueur: float, largeur: float):
         if longueur <= 0 or largeur <= 0:
@@ -43,21 +76,6 @@ class Rectangle(Forme, ABC):
     # OVERRIDES
     def __str__(self):
         return self.obtenir_description()
-
-
-class Cercle(Forme, ABC):
-    def __init__(self, rayon=1):
-        self.__rayon = rayon
-
-    def aire(self):
-        return math.pi * self.__rayon ** 2
-
-    def périmètre(self):
-        return 2 * math.pi * self.__rayon
-
-    def __str__(self):
-        return (f"Ceci est un rayon de {self.__rayon}m de rayon, avec une aire de {self.aire()}m2 et un périmètre de "
-                f"{self.périmètre()}m")
 
 
 if __name__ == '__main__':
