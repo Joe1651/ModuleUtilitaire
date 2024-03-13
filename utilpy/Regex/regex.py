bff89bc42075f88c1b800406e045a3e6599ed651
Joe1651 <jonathan.duplantis@hotmail.com>
2024-03-12 22:05:43 -0400

    Regex

--- utilpy/Regex/regex.py
+++ utilpy/Regex/regex.py
@@ -6,8 +6,14 @@ def code_postal_valide(code_postal:str):
 
 
 def num_téléphone_valide(num_téléphone: str):
-    return re.fullmatch(r"[(]?\d\d\d[)]?[ -]\d\d\d-\d\d\d\d", num_téléphone) is not None
+    return re.fullmatch(r"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$", num_téléphone) is not None
+
+
+def email_valide(email: str):
+    return re.fullmatch(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.["
+                        r"a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", email)
 
 
 assert code_postal_valide("J3L 0C6")
 assert num_téléphone_valide("450 447-7059")
+assert email_valide("jonathan.duplantis@hotmail.com")
